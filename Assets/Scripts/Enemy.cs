b0338d2b9c4dfa09233b3f2b832bcc57ff2fd395
Selthen <patopl2121@gmail.com>
2020-11-06 22:05:42 +0100

    *Spawning Area *Wave Manager *Enemy moving to location

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float speed = 2.0f;
+    private Rigidbody rigidbody;
+
+    private const float MOVEMENT_THRESHOLD = 2.0f;
+
+    public Vector3 Destination { get; set; }
+
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        Assert.IsNotNull(rigidbody);
+    }
+
+    private void Update()
+    {
+        if(Vector3.Distance(transform.position, Destination) > MOVEMENT_THRESHOLD)
+        {
+            rigidbody.velocity = (Destination - transform.position).normalized * speed;
+        }
+    }
+}
