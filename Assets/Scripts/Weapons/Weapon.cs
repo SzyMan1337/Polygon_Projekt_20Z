5f53a49c62d3d97083e907c2d4dcf5b0847069d0
piotr0sawicki <piotr0sawicki@gmail.com>
2020-11-07 15:32:02 +0100

    * Continuos shooting *

--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -6,6 +6,8 @@ public class Weapon : MonoBehaviour
 {
     [SerializeField] private Projectile projectilePrefab = null;
     [SerializeField] private Transform barrelEnd = null;
+    [SerializeField] private float reloadTime = 0.2f;
+    private float timeToShoot = 0.0f;
 
 
     private void Awake()
@@ -16,9 +18,14 @@ public class Weapon : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if ((Input.GetMouseButton(0)) && (timeToShoot <= 0.0f))
         {
+            timeToShoot = reloadTime;
             Instantiate(projectilePrefab, barrelEnd.position, barrelEnd.rotation);
+        }
+        else if(timeToShoot > 0)
+        {
+            timeToShoot -= Time.deltaTime;
         }
     }
 }
