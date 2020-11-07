636fe792c2587aa52d63037072a019ee4463e4f3
piotr0sawicki <piotr0sawicki@gmail.com>
2020-11-07 15:01:30 +0100

    * Prefab of weapon * * Prefab of projectile *

---
+++ Assets/Scripts/Weapons/Projectile.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float speed = 10.0f;
+    [SerializeField] private float lifetime = 5.0f;
+    private Rigidbody body = null;
+
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+        Assert.IsNotNull(body);
+        body.velocity = transform.forward * speed;
+    }
+
+    private void Update()
+    {
+        if(lifetime > 0.0f)
+        {
+            lifetime -= Time.deltaTime;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] private Projectile projectilePrefab = null;
+    [SerializeField] private Transform barrelEnd = null;
+
+
+    private void Awake()
+    {
+        Assert.IsNotNull(projectilePrefab);
+        Assert.IsNotNull(barrelEnd);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(projectilePrefab, barrelEnd.position, barrelEnd.rotation);
+        }
+    }
+}
