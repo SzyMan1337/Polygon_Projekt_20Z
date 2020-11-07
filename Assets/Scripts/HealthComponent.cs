826bcfa402ec9f3aa4eaebe306da0a7fdc1b3f43
Wooki <wooki1@op.pl>
2020-11-07 23:21:51 +0100

    * HealthComponent post merge fixes. * Removed unneccesary test classes.

--- Assets/Scripts/HealthComponent.cs
+++ Assets/Scripts/HealthComponent.cs
@@ -1,36 +1,33 @@
-﻿using Unity.Profiling;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Assertions;
-using System;
+
 
 public class HealthComponent : MonoBehaviour
 {
-    private float maxHealth = 10.0f;
-    [SerializeField] private float currentHealth = 0.0f;
-
-    public event EventHandler OnDeath;
+    [SerializeField] private float maxHealth = 10.0f;
+    private float currentHealth = 0.0f;
 
 
-    public void DeathMessage(object sender, EventArgs e)
+    public event System.Action OnDeath;
+
+
+    public bool IsAlive => currentHealth > 0.0f;
+
+
+    private void Awake()
     {
-        Debug.Log("You have been killed");
-    }
-
-    public void Awake()
-    {
-        OnDeath += DeathMessage;
         currentHealth = maxHealth;
     }
 
     public void ApplyDamage(float amount)
     {
         Assert.IsTrue(amount > 0.0f);
-        if (currentHealth > 0.0f)
+        if (IsAlive)
         {
-            currentHealth -= amount;
+            currentHealth = Mathf.Max(currentHealth - amount, 0.0f);
             if (currentHealth <= 0.0f)
             {
-                OnDeath?.Invoke(this, EventArgs.Empty); // invokes the event only if OnDeath is not null
+                OnDeath?.Invoke();
                 Destroy(gameObject);
             }  
         }
@@ -39,26 +36,9 @@ public class HealthComponent : MonoBehaviour
     public void ApplyHeal(float amount)
     {
         Assert.IsTrue(amount > 0.0f);
-        if (currentHealth + amount <= maxHealth)
+        if (IsAlive)
         {
-            currentHealth += amount;
+            currentHealth = Mathf.Min(currentHealth + amount, maxHealth);
         }
     }
-
-    public void DisplayHealth()
-    {
-        Debug.Log("Current health: " +  currentHealth + " Max health: " + maxHealth);
-    }
-
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            DisplayHealth();
-        }
-    }
-
-
-
 }
