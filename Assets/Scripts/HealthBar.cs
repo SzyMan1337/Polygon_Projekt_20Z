c18880f323d95881149b23ca26419259088e0e1c
Selthen <patopl2121@gmail.com>
2020-11-19 20:05:57 +0100

    *Created HealthBar *Deleted unwanted code in WaveManager

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Assertions;
+
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+    private HealthComponent healthComponent;
+
+
+    private void Awake()
+    {
+        Assert.IsNotNull(slider);
+        healthComponent = GetComponentInParent<HealthComponent>();
+        Assert.IsNotNull(healthComponent);
+
+        slider.maxValue = healthComponent.MaxHealth;
+        slider.value = healthComponent.CurrentHealth;
+    }
+
+    private void Update()
+    {
+        slider.value = healthComponent.CurrentHealth;
+    }
+}
--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -7,7 +7,6 @@ public class WaveManager : MonoBehaviour
 {
     [SerializeField] private Enemy enemyPrefab = null;
     [SerializeField] private BoxCollider[] spawnAreas;
-    [SerializeField] private Transform enemyMovementDestination = null;
     [SerializeField, Range(0.0f, 1000.0f)] private int numberOfEnemiesToSpawn = 0;
     private const float SPAWN_HEIGHT = 1.0f;
 
@@ -23,8 +22,6 @@ public class WaveManager : MonoBehaviour
             Assert.IsFalse(spawnAreasSet.Contains(spawnArea));
             spawnAreasSet.Add(spawnArea);
         }
-        //TODO: sprawdzanie duplikatów
-        Assert.IsNotNull(enemyMovementDestination);
     }
 
     private void Start()
@@ -36,7 +33,7 @@ public class WaveManager : MonoBehaviour
             var spawningArea = spawnAreas[Random.Range(0, spawnAreas.Length)];
             spawnPosition.x = spawningArea.bounds.extents.x * Random.Range(-1.0f, 1.0f) + spawningArea.transform.position.x;
             spawnPosition.z = spawningArea.bounds.extents.z * Random.Range(-1.0f, 1.0f) + spawningArea.transform.position.z;
-            var newEnemy = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+            Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
             --numberOfEnemiesToSpawn;
         }
     }
