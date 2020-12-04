72f64f978f12bcb47b0a1a4b3a47ce02db84a280
piotr0sawicki <piotr0sawicki@gmail.com>
2020-12-04 16:31:47 +0100

    * Wave manager * Invisible projectile and knife

--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -3,18 +3,40 @@ using UnityEngine.Assertions;
 using System.Collections.Generic;
 
 
+
+[System.Serializable]
+public class enemyType
+{
+    [SerializeField] Enemy prefab;
+    [SerializeField] int numberToSpawn = 0;
+
+
+    public int NumberToSpawn => numberToSpawn;
+    public Enemy Prefab => prefab;
+}
+
+
 public class WaveManager : MonoBehaviour
 {
-    [SerializeField] private Enemy enemyPrefab = null;
+    //[SerializeField] private Enemy enemyPrefab = null; //stare - pierwowzór
+   // [SerializeField, Range(0.0f, 1000.0f)] private int numberOfEnemiesToSpawn = 0; // rowniez
+    [SerializeField] enemyType[] enemiesToSpawn = new enemyType[3]; 
     [SerializeField] private BoxCollider[] spawnAreas;
-    [SerializeField, Range(0.0f, 1000.0f)] private int numberOfEnemiesToSpawn = 0;
+    
+ 
+    [SerializeField, Range(0.0f, 1000.0f)] private int numberOfShotgunsToSpawn = 0;
     private const float SPAWN_HEIGHT = 1.0f;
 
 
     private void Awake()
     {
-        Assert.IsNotNull(enemyPrefab); 
-        Assert.IsTrue(spawnAreas.Length > 0);
+        //Assert.IsNotNull(enemyPrefab); // piewowzor
+
+        foreach (var enemy in enemiesToSpawn)
+        {
+            Assert.IsNotNull(enemy.Prefab);
+        }
+            Assert.IsTrue(spawnAreas.Length > 0);
         var spawnAreasSet = new HashSet<BoxCollider>();
         foreach(var spawnArea in spawnAreas)
         {
@@ -28,13 +50,17 @@ public class WaveManager : MonoBehaviour
     {
         var spawnPosition = new Vector3(0.0f, SPAWN_HEIGHT, 0.0f);
 
-        while (numberOfEnemiesToSpawn > 0)
+        foreach (var type in enemiesToSpawn)
         {
-            var spawningArea = spawnAreas[Random.Range(0, spawnAreas.Length)];
-            spawnPosition.x = spawningArea.bounds.extents.x * Random.Range(-1.0f, 1.0f) + spawningArea.transform.position.x;
-            spawnPosition.z = spawningArea.bounds.extents.z * Random.Range(-1.0f, 1.0f) + spawningArea.transform.position.z;
-            var newEnemy = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
-            --numberOfEnemiesToSpawn;
+            var numberToSpawn = type.NumberToSpawn;
+            while (numberToSpawn > 0)
+            {
+                var spawningArea = spawnAreas[Random.Range(0, spawnAreas.Length)];
+                spawnPosition.x = spawningArea.bounds.extents.x * Random.Range(-1.0f, 1.0f) + spawningArea.transform.position.x;
+                spawnPosition.z = spawningArea.bounds.extents.z * Random.Range(-1.0f, 1.0f) + spawningArea.transform.position.z;
+                var newEnemy = Instantiate(type.Prefab, spawnPosition, Quaternion.identity);
+                --numberToSpawn;
+            }
         }
     }
 }
