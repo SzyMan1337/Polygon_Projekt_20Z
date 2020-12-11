1b42222dd3af0197545a7360aae9ca1016e6294f
piotr0sawicki <piotr0sawicki@gmail.com>
2020-12-11 16:38:03 +0100

    * BossEnemy death effect

--- Assets/Scripts/BossDeath.cs
+++ Assets/Scripts/BossDeath.cs
@@ -3,13 +3,14 @@
 public class BossDeath : MonoBehaviour
 { 
     public GameObject explosionEffect;
+    public GameObject effectHandler;
     [SerializeField] private float radius = 7.0f;
     [SerializeField] private float power = 500.0f;
 
     private void OnDestroy()
     {
         // Efekt wybuchu
-        //Instantiate(explosionEffect, transform.position, transform.rotation);
+        effectHandler = Instantiate(explosionEffect, transform.position, transform.rotation);
 
         Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
 
@@ -22,5 +23,6 @@ public class BossDeath : MonoBehaviour
                 health.ApplyDamage((radius - Vector3.Distance(collider.transform.position, transform.position)) * power);
             }
         }
+        Destroy(effectHandler, 2.0f);
     }
 }
